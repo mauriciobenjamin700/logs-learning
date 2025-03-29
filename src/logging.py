2e6d4352bb79e3f7ecb43d28c763e927dc2f626a
Mauricio Benjamin <mauriciobenjamin700@gmail.com>
2025-03-29 17:54:30 -0300

    doc: new docs to class

--- src/logging.py
+++ src/logging.py
@@ -6,6 +6,20 @@ class LogManager:
     A class to manage logging in the application.
     Provides methods to log messages at different levels (INFO, ERROR, etc.)
     to separate log files.
+    
+    - Attributes:
+        - _loggers:: dict: A dictionary to store logger instances.
+        
+    - Methods:
+        - _get_logger(level: str, file_path: str): 
+            Returns a logger instance for the specified level and file path.
+        - create_info_log(action: str, local: str, agente: str, agente_role: str, request_time: float):
+            Logs an INFO message to the info log file.
+        - create_error_log(action: str, local: str, agente: str, agente_role: str, status: int, detail: str):
+            Logs an ERROR message to the error log file.
+    - Example:
+        - LogManager.create_info_log("action", "local", "agente", "role", 0.123)
+        - LogManager.create_error_log("action", "local", "agente", "role", 404, "Not Found")
     """
     
     _loggers = {}
@@ -30,6 +44,15 @@ class LogManager:
     def create_info_log(cls, action: str, local: str, agente: str, agente_role: str, request_time: float):
         """
         Log an INFO message.
+        
+        - Args:
+            - action:: str: The action being logged.
+            - local:: str: The local context of the action.
+            - agente:: str: The agent involved in the action.
+            - agente_role:: str: The role of the agent.
+            - request_time:: float: The time taken for the request.
+        - Returns:
+            - None
         """
         log_message = f"{action}---{local}---{agente}---{agente_role}---{request_time}"
         logger = cls._get_logger("INFO", "src/logs/info.log")
@@ -39,6 +62,16 @@ class LogManager:
     def create_error_log(cls, action: str, local: str, agente: str, agente_role: str, status: int, detail: str):
         """
         Log an ERROR message.
+        
+        - Args:
+            - action:: str: The action being logged.
+            - local:: str: The local context of the action.
+            - agente:: str: The agent involved in the action.
+            - agente_role:: str: The role of the agent.
+            - status:: int: The HTTP status code.
+            - detail:: str: Additional details about the error.
+        - Returns:
+            - None
         """
         log_message = f"{action}---{local}---{agente}---{agente_role}---{status}---{detail}"
         logger = cls._get_logger("ERROR", "src/logs/error.log")
